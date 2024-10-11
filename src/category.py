0903ee1386a90c8e86ec56f082f4863867a15281
Vladimir Inozemtsev <vladimir.inozemtzev@yandex.ru>
2024-10-11 22:49:56 +0300

    test update

--- src/category.py
+++ src/category.py
@@ -126,7 +126,7 @@ class Category:
         """
         product_strings = []
         for product in self.__products:
-            product_strings.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n")
+            product_strings.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.")
         return product_strings
 
     def __str__(self):
@@ -137,63 +137,65 @@ class Category:
         products_str = "\n".join(f"  - {product}" for product in self.__products)
         return f"{self.name}:\n{products_str}"
 
-    class CategoryIterator:
+
+class CategoryIterator:
+    """
+    Итератор для перебора товаров в категории.
+    """
+
+    def __init__(self, category: Category):
         """
-        Итератор для перебора товаров в категории.
+        Инициализация итератора.
+
+        Args:
+            category: Объект класса Category.
         """
+        self.category = category
+        self.index = 0
 
-        def __init__(self, category: Category):
-            """
-            Инициализация итератора.
+    def __iter__(self):
+        """
+        Возвращает сам итератор.
+        """
+        return self
 
-            Args:
-                category: Объект класса Category.
-            """
-            self.category = category
-            self.index = 0
+    def __next__(self):
+        """
+        Возвращает следующий товар из категории.
 
-        def __iter__(self):
-            """
-            Возвращает сам итератор.
-            """
-            return self
+        Returns:
+            Объект класса Product.
+        """
+        if self.index < len(self.category.products):
+            product = self.category.products[self.index]
+            self.index += 1
+            return product
+        else:
+            raise StopIteration
 
-        def __next__(self):
-            """
-            Возвращает следующий товар из категории.
 
-            Returns:
-                Объект класса Product.
-            """
-            if self.index < len(self.category.__products):
-                product = self.category.__products[self.index]
-                self.index += 1
-                return product
-            else:
-                raise StopIteration
+if __name__ == "__main__":
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
-    if __name__ == '__main__':
-        product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-        product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-        product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+    print(str(product1))
+    print(str(product2))
+    print(str(product3))
 
-        print(str(product1))
-        print(str(product2))
-        print(str(product3))
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
 
-        category1 = Category(
-            "Смартфоны",
-            "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-            [product1, product2, product3]
-        )
+    print(str(category1))
 
-        print(str(category1))
+    print(category1.products)
 
-        print(category1.products)
+    print(product1 + product2)
+    print(product1 + product3)
+    print(product2 + product3)
 
-        print(product1 + product2)
-        print(product1 + product3)
-        print(product2 + product3)
-
-        for product in CategoryIterator(category1):
-            print(product)
+    for product in CategoryIterator(category1):
+        print(product)
