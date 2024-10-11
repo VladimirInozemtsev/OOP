588123a3f382cba55ada43cb62de862ab0d64ab0
Vladimir Inozemtsev <vladimir.inozemtzev@yandex.ru>
2024-10-11 19:00:09 +0300

    Magical methods str, add

--- src/category.py
+++ src/category.py
@@ -66,6 +66,26 @@ class Product:
 
         return cls(name, description, price, quantity)
 
+    def __str__(self):
+        """
+        Возвращает строковое представление объекта Product.
+        """
+        return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other):
+        """
+        Сложение товаров: возвращает сумму стоимости товаров на складе.
+
+        Args:
+            other: Любой объект Product.
+
+        Returns:
+            Сумма стоимости товаров (float).
+        """
+        if not isinstance(other, Product):
+            raise TypeError("Складывать можно только объекты Product.")
+        return self.price * self.quantity + other.price * other.quantity
+
 
 class Category:
     """
@@ -109,42 +129,71 @@ class Category:
             product_strings.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n")
         return product_strings
 
+    def __str__(self):
+        """
+        Возвращает строковое представление объекта Category.
 
-if __name__ == "__main__":
+        """
+        products_str = "\n".join(f"  - {product}" for product in self.__products)
+        return f"{self.name}:\n{products_str}"
 
-    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+    class CategoryIterator:
+        """
+        Итератор для перебора товаров в категории.
+        """
 
-    category1 = Category(
-        "Смартфоны",
-        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-        [product1, product2, product3],
-    )
+        def __init__(self, category: Category):
+            """
+            Инициализация итератора.
 
-    print(category1.products)
-    product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
-    category1.add_product(product4)
-    print(category1.products)
-    print(category1.product_count)
+            Args:
+                category: Объект класса Category.
+            """
+            self.category = category
+            self.index = 0
 
-    new_product = Product.new_product(
-        {
-            "name": "Samsung Galaxy S23 Ultra",
-            "description": "256GB, Серый цвет, 200MP камера",
-            "price": 180000.0,
-            "quantity": 5,
-        }
-    )
-    print(new_product.name)
-    print(new_product.description)
-    print(new_product.price)
-    print(new_product.quantity)
+        def __iter__(self):
+            """
+            Возвращает сам итератор.
+            """
+            return self
 
-    new_product.price = 800
-    print(new_product.price)
+        def __next__(self):
+            """
+            Возвращает следующий товар из категории.
 
-    new_product.price = -100
-    print(new_product.price)
-    new_product.price = 0
-    print(new_product.price)
+            Returns:
+                Объект класса Product.
+            """
+            if self.index < len(self.category.__products):
+                product = self.category.__products[self.index]
+                self.index += 1
+                return product
+            else:
+                raise StopIteration
+
+    if __name__ == '__main__':
+        product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+        product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+        product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+        print(str(product1))
+        print(str(product2))
+        print(str(product3))
+
+        category1 = Category(
+            "Смартфоны",
+            "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+            [product1, product2, product3]
+        )
+
+        print(str(category1))
+
+        print(category1.products)
+
+        print(product1 + product2)
+        print(product1 + product3)
+        print(product2 + product3)
+
+        for product in CategoryIterator(category1):
+            print(product)
